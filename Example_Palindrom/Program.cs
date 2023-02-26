90d9442c10ab31432ecde1c987f229cc80e76793
Julyidol <lineoff@mail.com>
2023-02-26 18:57:56 +0300

    Добавили проверку палиндромов

---
+++ Example_Palindrom/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, палиндромом ли это
+
+Console.Write("Проверим палиндром ли это? Введи пятизначное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] ==number[4]  && number [1] == number[3])
+    {
+        Console.WriteLine("Это палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Это не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Это не пятизначное число");
+}
